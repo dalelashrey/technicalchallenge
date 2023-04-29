3c89e5598401a65649b0d519c88867b667a9648b
Shrey <shrey@outlook.com>
2023-04-29 17:06:20 +0530

    Add files via upload

---
+++ Chalenge2.ps1
@@ -0,0 +1,37 @@
+﻿# Import the required Azure AD and Graph API modules
+Import-Module AzureADPreview
+Import-Module AzureAD
+Import-Module AzureRM.Profile
+
+# Set the subscription context
+Set-AzContext -SubscriptionId "replace_with_subscription_id"
+
+# Authenticate with Azure AD
+Connect-AzureAD
+
+# Get the VM name as input
+$vmName = Read-Host "Enter the name of the VM"
+
+# Get the VM resource ID
+$vmResourceId = (Get-AzVM -Name $vmName).Id
+
+# Define the Graph API endpoint for the VM
+$graphApiEndpoint = "https://graph.microsoft.com/v1.0/$vmResourceId"
+
+# Retrieve the VM metadata using Graph API
+$vmMetadata = Invoke-RestMethod -Headers @{Authorization = "Bearer $((Get-AzureADAccessToken -Resource 'https://graph.microsoft.com').AccessToken)"} -Uri $graphApiEndpoint
+
+# Convert the metadata to JSON format
+$metadataJson = $vmMetadata | ConvertTo-Json
+
+# Output the JSON
+Write-Output $metadataJson
+
+# Get the data key as input
+$dataKey = Read-Host "Enter the data key to retrieve (or press enter to skip)"
+
+# Retrieve the value of the data key if it is provided
+if ($dataKey -ne "") {
+    $dataValue = $vmMetadata.$dataKey
+    Write-Output "$dataKey: $dataValue"
+}
